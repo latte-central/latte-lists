1d3df5393c262cfe8ebdfbee26d8278e05576c6f
Frederic Peschanski <frederic.peschanski@lip6.fr>
2023-11-28 14:07:27 +0100

    append null right

--- src/latte_lists/append.clj
+++ src/latte_lists/append.clj
@@ -36,11 +36,7 @@
   [[?T :type] [ys (list T)]]
   (q/the (append-unique ys)))
 
-(definition append
-  [[?T :type] [xs ys (list T)]]
-  ((append-fun xs) ys))
-
-(defthm append-prop
+(defthm append-fun-prop
   [[?T :type] [ys (list T)]]
   (and (equal ((append-fun ys) (null T)) ys)
        (forall [xs (list T)]
@@ -48,15 +44,64 @@
            (equal ((append-fun ys) (cons x xs))
                   (cons x ((append-fun ys) xs)))))))
 
-(proof 'append-prop-thm
+(proof 'append-fun-prop-thm
   (qed (q/the-prop (append-unique ys))))
 
-(defthm append-null
+(definition append
+  [[?T :type] [xs ys (list T)]]
+  ((append-fun ys) xs))
+
+(defthm append-null-left
+  [[?T :type] [xs (list T)]]
+  (equal (append (null T) xs) xs))
+
+(proof 'append-null-left-thm
+  (qed (p/and-elim-left (append-fun-prop xs))))
+
+(defthm append-cons
+  [[?T :type] [x T] [xs ys (list T)]]
+  (equal (append (cons x xs) ys)
+         (cons x (append xs ys))))
+
+(proof 'append-cons-thm
+  (qed ((p/and-elim-right (append-fun-prop ys)) xs x)))
+
+(defthm append-null-right
   [[?T :type] [xs (list T)]]
   (equal (append xs (null T)) xs))
 
-(proof 'append-null-thm
-  (qed (p/and-elim-left (append-prop xs))))
+(proof 'append-null-right-thm
+  (pose P := (lambda [xs (list T)] 
+               (equal (append xs (null T)) xs)))
+  
+  "By structural induction"
+
+  "case (null T)"
+  (have <a> (P (null T))
+        :by (append-null-left (null T)))
+
+  "inductive case"
+
+  (assume [xs (list T)
+           Hind (P xs)
+           x T]
+    "Now we have to prove (P (cons x xs))"
+    
+
+    (have <b1> (equal (cons x (append xs (null T)))
+                      (cons x xs))
+          :by (eq/eq-cong (lambda [$ (list T)] (cons x $)) Hind))
+
+    (have <b2> (equal (append (cons x xs) (null T))
+                      (cons x (append xs (null T))))
+          :by (append-cons x xs (null T)))
+    
+    (have <b> (P (cons x xs)) :by (eq/eq-trans <b2> <b1>)))
+
+  "Use the induction principle"
+  (qed ((lists/list-induct P) <a> <b> xs)))
+
+
 
 
 
